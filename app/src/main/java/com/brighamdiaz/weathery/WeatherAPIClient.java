cc8ca34d631e818644a403f2195a2f3ec4bfc670
Brigham Diaz <brigham.diaz@gmail.com>
2016-11-30 08:11:56 -0700

    moved forecast request to client

--- app/src/main/java/com/brighamdiaz/weathery/WeatherAPIClient.java
+++ app/src/main/java/com/brighamdiaz/weathery/WeatherAPIClient.java
@@ -1,5 +1,16 @@
 package com.brighamdiaz.weathery;
 
+import android.content.Context;
+
+import com.brighamdiaz.weathery.model.Forecast;
+import com.brighamdiaz.weathery.model.ForecastResponse;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,6 +23,7 @@ public class WeatherAPIClient {
     // base URL for all requests
     public static final String YAHOO_URL = "https://query.yahooapis.com/v1/public/";
     private static Retrofit mRetrofit;
+    private OnForecastReceivedListener mListener;
 
     /**
      * Create a new Retrofit Yahoo weather client
@@ -27,4 +39,37 @@ public class WeatherAPIClient {
 
         return mRetrofit;
     }
+
+    public void requestForecast(Context context) {
+        if(context instanceof OnForecastReceivedListener) {
+            mListener = (OnForecastReceivedListener) context;
+        }
+        WeatherAPIInterface apiService = WeatherAPIClient.getClient().create(WeatherAPIInterface.class);
+        // insert items into a linked hash map to preserve insertion order. Do not set encoded=true
+        Map<String, String> query = new LinkedHashMap<>();
+        query.put("q", "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"nome, AK\")");
+        query.put("format", "json");
+        query.put("env", "store://datatables.org/alltableswithkeys");
+
+        Call<ForecastResponse> call = apiService.getForecast(query);
+        call.enqueue(new Callback<ForecastResponse>() {
+            @Override
+            public void onResponse(Call<ForecastResponse> call, Response<ForecastResponse> response) {
+                if(mListener != null) {
+                    mListener.onWeatherDataReceived(response.body().getWeatherResult());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ForecastResponse> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+
+    public interface OnForecastReceivedListener {
+        void onWeatherDataReceived(Forecast forecast);
+    }
+
 }
